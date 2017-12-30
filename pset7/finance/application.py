036f2b396b955fd6348407bf49d0f0f8a2e38d4a
deep <ddiwani3008@gmail.com>
2017-12-30 15:59:46 +0530

    index and buy feature added

--- pset7/finance/application.py
+++ pset7/finance/application.py
@@ -7,7 +7,7 @@ from tempfile import mkdtemp
 
 from helpers import *
 
-# connect to database, define cursor 
+# connect to database, define cursor
 db = sqlite3.connect("finance.db")
 crsr = db.cursor()
 
@@ -35,25 +35,40 @@ Session(app)
 # configure CS50 Library to use SQLite database
 #db = SQL("sqlite:///finance.db")
 
-# ---------------------------------------------INDEX---------------------------------------------
-@app.route("/")
-@login_required
-def index():
-    return error("TODO")
 
-# ---------------------------------------------BUY---------------------------------------------
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """Buy shares of stock."""
-    return error("TODO")
+# ---------------------------------------------REGISTER---------------------------------------------
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user."""
 
-# ---------------------------------------------HISTORY---------------------------------------------
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions."""
-    return error("TODO")
+    # if request method is GET, load register.html page
+    if request.method == "GET":
+        return render_template("register.html")
+
+    # else submit the form
+    else:
+        if not request.form.get("username"):
+            return error("wow! you don't want to provide username")
+        elif not request.form.get("password"):
+            return error("wow! you don't want to keep password")
+        elif not request.form.get("password2"):
+            return error("dude! Re-enter the password")
+        elif request.form.get("password") != request.form.get("password2"):
+            return error("Password did not match")
+
+        # look if username exist
+        crsr.execute("SELECT * FROM users WHERE username=(?)", (request.form.get("username"),) )
+
+        # if exist then return error
+        if crsr.fetchone() != None:
+            return error("Username already exist")
+        else:
+            # add username and hash password in database
+            crsr.execute("INSERT INTO users (username, hash) VALUES ((?), (?));",
+                ( request.form.get("username"), pwd_context.encrypt(request.form.get("password")), ) )
+            db.commit()
+            return redirect(url_for("index"))
+
 
 # ---------------------------------------------LOGIN---------------------------------------------
 @app.route("/login", methods=["GET", "POST"])
@@ -75,7 +90,7 @@ def login():
             return error("must provide password")
 
         # query database for username
-        crsr.execute("SELECT * FROM users WHERE username = (?)", (request.form.get("username"),) ) 
+        crsr.execute("SELECT * FROM users WHERE username = (?);", (request.form.get("username"),) )
         row = crsr.fetchone()
 
         # ensure username exists and password is correct
@@ -92,6 +107,107 @@ def login():
     else:
         return render_template("login.html")
 
+
+# ---------------------------------------------INDEX---------------------------------------------
+@app.route("/")
+@login_required
+def index():
+    symbol = []
+    share = []
+    price = []
+
+    crsr.execute("SELECT symbol,newHolding FROM history WHERE id=(?) ORDER BY time DESC;", (session["user_id"],) )
+    transactionHistory = crsr.fetchall()
+
+    for row in transactionHistory:
+        if int(row[1])>0 and row[0] not in symbol:
+            symbol.append(row[0])
+            share.append(int(row[1]))
+            currentPrice = lookup(row[0])
+            price.append(currentPrice["price"])
+
+    return render_template("index.html", symbol=symbol, share=share, price=price, size=len(symbol))
+
+
+# ---------------------------------------------QUOTE---------------------------------------------
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "GET":
+        return render_template("quote.html")
+    else:
+        quoteResult = lookup(request.form.get("symbol"))
+
+        if quoteResult == None:
+            return error("Error occurred in retrieving cost")
+        else:
+            return render_template("quoted.html", symbol=quoteResult["symbol"], name=quoteResult["name"], price=quoteResult["price"])
+
+
+# ---------------------------------------------BUY---------------------------------------------
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    # Buy shares of stock
+
+    if request.method == "GET":
+        # rendering buying page
+        return render_template("buy.html")
+    else:
+        # buying stocks
+        quoteResult = lookup(request.form.get("symbol"))
+
+        # if error occured in retrieving cost
+        if quoteResult == None:
+            return error("Error occurred in retrieving cost")
+
+        crsr.execute("SELECT cash FROM users WHERE id=(?);", (session["user_id"],) )
+        # money is a tuple here, use money[0] to access amount
+        money = crsr.fetchone()
+
+        print("balance:{} bill:{}".format(money[0], float(request.form.get("quantity"))*float(quoteResult["price"]) ))
+
+        # abort if user do not have enough money
+        if money[0] < float(request.form.get("quantity"))*float(quoteResult["price"]):
+            print("Don't have enough cash")
+            return error("Don't have enough cash")
+
+        crsr.execute("SELECT newHolding FROM history WHERE id=(?) AND symbol=(?) ORDER BY time DESC;", (session["user_id"], request.form.get("symbol"),) )
+        prevData = crsr.fetchone()
+
+        if prevData == None:
+            prevHolding = 0
+        else:
+            prevHolding = int(prevData[0])
+
+        print("prevHolding={}".format(prevHolding))
+        print("id={}, symbol={}, rate={}, quantity={}, newHolding={}".format(type(session["user_id"]), type(request.form.get("symbol")), type(quoteResult["price"]), type(request.form.get("quantity")), type(request.form.get("quantity")+prevHolding),) )
+
+        crsr.execute("INSERT INTO history (id, symbol, rate, quantity, newHolding) VALUES ((?), (?), (?), (?), (?));", \
+            (session["user_id"], request.form.get("symbol"), float(quoteResult["price"]), request.form.get("quantity"), request.form.get("quantity")+prevHolding,) )
+
+        print("updated cash={}".format(money[0]-float(request.form.get("quantity"))*float(quoteResult["price"]) ) )
+        crsr.execute("UPDATE users SET cash=(?) WHERE id=(?);", (money[0]-float(request.form.get("quantity"))*float(quoteResult["price"]), session["user_id"],) )
+        db.commit()
+        return error("SUCCESSFULLY BOUGHT")
+
+# ---------------------------------------------SELL---------------------------------------------
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock."""
+    return error("TODO")
+
+
+# ---------------------------------------------HISTORY---------------------------------------------
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions."""
+    return error("TODO")
+
+
 # ---------------------------------------------LOGOUT---------------------------------------------
 @app.route("/logout")
 def logout():
@@ -102,58 +218,3 @@ def logout():
 
     # redirect user to login form
     return redirect(url_for("login"))
-
-# ---------------------------------------------QUOTE---------------------------------------------
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    """Get stock quote."""
-    if request.method == "GET":
-        return render_template("quote.html")
-    else:
-        quoteResult = lookup(request.form.get("symbol"))     
-        
-        if quoteResult == None:
-            return error("Error occurred in retrieving cost")
-        else           
-            return render_template("quoted.html", symbol=quoteResult["symbol"], name=quoteResult["name"], price=quoteResult["price"])
-# ---------------------------------------------REGISTER---------------------------------------------
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user."""
-
-    # if request method is GET, load register.html page
-    if request.method == "GET":
-        return render_template("register.html")
-
-    # else submit the form    
-    else:
-        if not request.form.get("username"):
-            return error("wow! you don't want to provide username")
-        elif not request.form.get("password"):
-            return error("wow! you don't want to keep password")
-        elif not request.form.get("password2"):
-            return error("dude! Re-enter the password")    
-        elif request.form.get("password") != request.form.get("password2"):
-            return error("Password did not match")    
-        
-        # look if username exist
-        crsr.execute("SELECT * FROM users WHERE username=(?)", (request.form.get("username"),) )
-
-        # if exist then return error
-        if crsr.fetchone() != None:
-            return error("Username already exist")
-        else:
-            # add username and hash password in database
-            crsr.execute("INSERT INTO users (username, hash) VALUES ((?), (?));", 
-                ( request.form.get("username"), pwd_context.encrypt(request.form.get("password")), ) )
-            db.commit()
-            return redirect(url_for("index"))
-
-            
-# ---------------------------------------------SELL---------------------------------------------
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    """Sell shares of stock."""
-    return error("TODO")
